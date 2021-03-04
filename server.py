b6146da0f5e559e207c404d181242cab17acf38f
Mikhail <48692715+Mikhail-Gl96@users.noreply.github.com>
2021-03-05 02:07:14 +0300

    del comments rewrite app args reformat code

--- server.py
+++ server.py
@@ -6,41 +6,43 @@ import os
 from aiohttp import web
 
 
-CHUNK_SIZE = 1024 * 100  # 100 kb
+CHUNK_SIZE = 1024 * 100
 
 
 async def archivate(request):
-    photos_path = request.app.args.path
+    photos_path = request.app.args['path']
     archive_name = request.match_info['archive_hash']
     if os.path.exists(os.path.join(photos_path, archive_name)) is False:
-        if request.app.args.logging:
+        if request.app.args['logging']:
             logging.error(f'no archive with name: {archive_name}')
-        raise web.HTTPNotFound(text='Такого архива нет', content_type='text/html')
+        raise web.HTTPNotFound(text='Такого архива нет',
+                               content_type='text/html')
     response = web.StreamResponse()
-    response.headers['Content-Disposition'] = f'attachment; filename="{archive_name}.zip'
+    attachment = f'attachment; filename="{archive_name}.zip'
+    response.headers['Content-Disposition'] = attachment
     await response.prepare(request)
-    if request.app.args.logging:
+    if request.app.args['logging']:
         logging.debug(f'send headers')
-    process = await asyncio.create_subprocess_exec('zip', *['-r', '-', archive_name],
-                                                   cwd=photos_path,
-                                                   stdout=asyncio.subprocess.PIPE,
-                                                   stderr=asyncio.subprocess.PIPE)
+    process = await asyncio.create_subprocess_exec(
+        'zip',
+        *['-r', '-', archive_name],
+        cwd=photos_path,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE)
     try:
         while True:
             stdout = await process.stdout.read(CHUNK_SIZE)
-            if request.app.args.logging:
+            if request.app.args['logging']:
                 logging.info('Sending archive chunk ...')
             await response.write(stdout)
-            if request.app.args.low_speed:
-                # low speed on
+            if request.app.args['low_speed']:
                 await asyncio.sleep(2)
-                # stop low speed on
             if process.stdout.at_eof():
-                if request.app.args.logging:
+                if request.app.args['logging']:
                     logging.info('Send all')
                 break
     except asyncio.CancelledError:
-        if request.app.args.logging:
+        if request.app.args['logging']:
             logging.error('Download was interrupted')
         process.kill()
         await process.communicate()
@@ -49,27 +51,43 @@ async def archivate(request):
 
 
 async def handle_index_page(request):
-    async with aiofiles.open('index.html', mode='r', encoding='utf-8') as index_file:
+    async with aiofiles.open('index.html',
+                             mode='r',
+                             encoding='utf-8') as index_file:
         index_contents = await index_file.read()
-    return web.Response(text=index_contents, content_type='text/html')
+    return web.Response(text=index_contents,
+                        content_type='text/html')
 
 
 def get_args():
     parser = argparse.ArgumentParser(description='async photo server.')
-    parser.add_argument('-log', '--logging', action='store_true', help='logging mode on/off')
-    parser.add_argument('-s', '--low_speed', action='store_true', help='low speed mode on/off')
-    parser.add_argument('-p', '--path', type=str, help='path to photos on/off', default="test_photos")
+    parser.add_argument('-log',
+                        '--logging',
+                        action='store_true',
+                        help='logging mode on/off')
+    parser.add_argument('-s',
+                        '--low_speed',
+                        action='store_true',
+                        help='low speed mode on/off')
+    parser.add_argument('-p',
+                        '--path',
+                        type=str,
+                        help='path to photos on/off',
+                        default="test_photos")
     args = parser.parse_args()
     return args
 
 
 if __name__ == '__main__':
-    # python3 server.py -log False -s  False
     app_args = get_args()
     logging.basicConfig(level=logging.INFO)
 
     app = web.Application()
-    app.args = app_args
+    app.args = {
+        'path': app_args.path,
+        'low_speed': app_args.low_speed,
+        'logging': app_args.logging
+    }
     app.add_routes([
         web.get('/', handle_index_page),
         web.get('/archive/{archive_hash}/', archivate)
